17912a1b560d7533407e6209d3da8cf86cc85fc4
luc-nguyen-174 <louiskingvn@gmail.com>
2023-04-06 16:45:06 +0700

    done

--- src/main/java/com/demoajax/controller/SmartphoneController.java
+++ src/main/java/com/demoajax/controller/SmartphoneController.java
@@ -8,13 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/smartphones")
+@CrossOrigin("*")
 public class SmartphoneController {
     @Autowired
     private ISmartphoneService smartphoneService;
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Smartphone> createSmartphone(@RequestBody Smartphone smartphone) {
         return new ResponseEntity<>(smartphoneService.save(smartphone), HttpStatus.CREATED);
     }
@@ -28,5 +31,15 @@ public class SmartphoneController {
     @GetMapping
     public ResponseEntity<Iterable<Smartphone>> allPhones() {
         return new ResponseEntity<>(smartphoneService.findAll(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Smartphone> deleteSmartphone(@PathVariable Long id) {
+        Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
+        if (!smartphoneOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        smartphoneService.remove(id);
+        return new ResponseEntity<>(smartphoneOptional.get(), HttpStatus.NO_CONTENT);
     }
 }
