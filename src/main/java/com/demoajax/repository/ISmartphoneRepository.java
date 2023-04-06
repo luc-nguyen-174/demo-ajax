3d1b007f95306bd84c621d70f75b197f027506fc
luc-nguyen-174 <louiskingvn@gmail.com>
2023-04-06 11:33:19 +0700

    first commit

---
+++ src/main/java/com/demoajax/repository/ISmartphoneRepository.java
@@ -0,0 +1,9 @@
+package com.demoajax.repository;
+
+import com.demoajax.model.Smartphone;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISmartphoneRepository extends CrudRepository<Smartphone, Long> {
+}
---
+++ src/main/java/com/demoajax/service/ISmartphoneService.java
@@ -0,0 +1,15 @@
+package com.demoajax.service;
+
+import com.demoajax.model.Smartphone;
+
+import java.util.Optional;
+
+public interface ISmartphoneService {
+    Iterable<Smartphone> findAll();
+
+    Optional<Smartphone> findById(Long id);
+
+    Smartphone save(Smartphone smartPhone);
+
+    void remove(Long id);
+}
